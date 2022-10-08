6ced7a85a451c9e2c0931c162c20616e6354e086
florian <martinflorian2@gmail.com>
2022-10-08 09:43:47 +0200

    delete blog feature

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlog] = useState([
+    const [blogs, setBlogs] = useState([
         { title: "light saber prices go sky rocket", body: "Somewhere in the wild, the beast is waiting to feed.", author: "John Travolta", id: 1 },
         { title: "Check out my new pizza restaurant", body: "Somewhere in the wild, the beast is waiting to feed.", author: "Benito Mussolini", id: 2 },
         { title: "Donald Trump was actually a woman", body: "Somewhere in the wild, the beast is waiting to feed.", author: "Oliver Twist", id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"Blogs list"} />
+            <BlogList blogs={blogs} title={"Blogs list"} handleDelete={handleDelete}/>
         </div>
     );
 }
