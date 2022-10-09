14bd9fbdd702f96c35e767780ff0b73ddee5d020
florian <martinflorian2@gmail.com>
2022-10-09 15:13:30 +0200

    switch to custom hook for home.js

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
 
-    const [isPending, setIsPending] = useState(true);
-
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw new Error(`Could not fetch data from that resource`);
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-
-                })
-        }, 1000);
-    }, []);
+    const {data: blogs, isPending, error} = useFetch(`http://localhost:8000/blogs`);
 
     return (
         <div className="home">
