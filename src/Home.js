8b7e0a3d98e738b9139899e5d0320ced206ea6b8
florian <martinflorian2@gmail.com>
2022-10-09 14:53:06 +0200

    handling server and endpoint errors

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,37 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
+    const [isPending, setIsPending] = useState(true);
+
+    const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data)
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok) {
+                        throw new Error(`Could not fetch data from that resource`);
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
             {blogs && <BlogList blogs={blogs} title={"Blogs list"} />}
         </div>
     );
