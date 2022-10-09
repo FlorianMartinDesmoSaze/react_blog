698a77857ecf207f0ccff6eef216ba9308e54c3f
florian <martinflorian2@gmail.com>
2022-10-09 12:51:29 +0200

    fetching data useEffect & remove delete blog

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,22 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "light saber prices go sky rocket", body: "Somewhere in the wild, the beast is waiting to feed.", author: "John Travolta", id: 1 },
-        { title: "Check out my new pizza restaurant", body: "Somewhere in the wild, the beast is waiting to feed.", author: "Benito Mussolini", id: 2 },
-        { title: "Donald Trump was actually a woman", body: "Somewhere in the wild, the beast is waiting to feed.", author: "Oliver Twist", id: 3 }
-    ]);
-
-    const [name, setName] = useState("Takeshi");
-
- 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect log ', blogs);
-        console.log(name);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data)
+            })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title={"Blogs list"} handleDelete={handleDelete}/>
-            <p>{name}</p>
-            <button onClick={() => setName('Bob')}>Change name</button>
+            {blogs && <BlogList blogs={blogs} title={"Blogs list"} />}
         </div>
     );
 }
