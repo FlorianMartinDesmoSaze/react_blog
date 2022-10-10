dd36d2d385285ff98ef8b08ac27258ed08e57d62
florian <martinflorian2@gmail.com>
2022-10-10 12:44:52 +0200

    blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,27 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
 const BlogDetails = () => {
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch("http://localhost:8000/blogs/" + id);
+
+    console.log(blog);
+
     return (
         <div className="blog-details">
-            <h2>Blog Details</h2>
+            <h2>Blog #{id} Details</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{ blog.body }</div>
+                </article>
+            )}
+            {/* <div className="blog-preview" key={blog.id}>
+                        <h2 className="blog-title">{ blog.title }</h2>
+                    </div> */}
         </div>
     );
 }
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,19 @@
-const BlogList = ({blogs, title}) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs, title }) => {
     // const blogs = props.blogs;
     // const title = props.title;
     // const {blogs} = props;
-
+console.log(blogs);
     return (
         <div className="blog-list">
             <h2> {title} </h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`} style={{textDecoration: "none"}}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
